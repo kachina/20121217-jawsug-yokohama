84f0abed54819653ee11f279cfb873aba56978b1
kachina <kachina.t@gmail.com>
2012-12-15 08:59:52 +0000

    AccessKey,SecretAccessKeyが必要なパタンのサンプルソールを作成しました

---
+++ sample_1.php
@@ -0,0 +1,16 @@
+<?php
+  require_once('AWSSDKforPHP/sdk.class.php');
+
+  $credential = array(
+                  'key'    => 'Access Key',
+                  'secret' => 'Secret Access Key'
+                );
+
+  $instance_id = file_get_contents('http://169.254.169.254/latest/meta-data/instance-id');
+  $ec2 = new AmazonEC2($credential);
+  $ec2->set_region(AmazonEC2::REGION_TOKYO);
+  $response = $ec2->describe_instances(array('InstanceId' => $instance_id));
+  $instance = $response->body->reservationSet->item->instancesSet->item;
+
+  echo 'Instance Id = [' . $instance->instanceId . ']<br />';
+  echo 'DNS Name = [' . $instance->dnsName . ']<br />';
